101979e973d563e2329c526f5e067f73a292ec9d
Matt Olm <mattolm@Matts-MacBook-Pro.local>
2021-10-28 13:47:53 -0700

    0.0.1

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from py_sa._version import __version__
 
 setup(name='py_sa',
       version=__version__,
       description='Interact with aegea and s3 through python',
+      packages=find_packages(),
       author='Matt Olm',
       author_email='mattolm@stanford.edu',
       license='MIT',
-      packages=['py_sa'],
       install_requires=[
           'pandas',
           'boto3'
